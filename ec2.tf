100d182a0eda2f99ed91283048167cbdab926bfb
Ann Kim <annkim@MacBookPro.fritz.box>
2026-04-29 02:18:33 +0200

    ec2

--- ec2.tf
+++ ec2.tf
@@ -11,3 +11,15 @@ resource "aws_instance" "petsearch_wordpress_server" {
     Name = "petseach-wordpress-server"
   }
 }
+resource "aws_instance" "bastion-instance" {
+  ami                         = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.public_subnet_petsearch_1
+  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+
+  tags = {
+    Name = "bastion-instance"
+  }
+}
