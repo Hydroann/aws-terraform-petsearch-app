aaae741c3071b4450625e98d717de3a53d3ea4e6
Ann Kim <annkim@MacBookPro.fritz.box>
2026-04-30 15:12:37 +0200

    rds updates

---
+++ rds.tf
@@ -0,0 +1,19 @@
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [private_petsearch_subnet_1]
+}
+
+resource "aws_db_instance" "rds_instance" {
+  identifier              = "petsearch-rds-instance"
+  engine                  = "mysql"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+  username              = var.db_username
+  password              = var.db_password
+  multi_az                = true
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+}
+
