a1313e38098e9ff06838b32d28f214a7938073c3
Ann Kim <annkim@MacBookPro.fritz.box>
2026-05-03 17:06:54 +0200

    errors handled

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "petsearch_webserver" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  subnet_id                   = aws_subnet.public_petsearch_subnet_1
-  vpc_security_group_ids      = [aws_security_group.petsearch_web_sg]
+  subnet_id                   = aws_subnet.public-petsearch-subnet-1
+  vpc_security_group_ids      = [aws_security_group.petsearch-web-sg]
   associate_public_ip_address = true
   key_name                    = var.key_name
   user_data                   = file("scripts/userdata.sh")
@@ -16,8 +16,8 @@ resource "aws_instance" "bastion-instance" {
   instance_type               = var.instance_type
   associate_public_ip_address = true
   key_name                    = var.key_name
-  subnet_id                   = aws_subnet.public_petsearch_subnet_1
-  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  subnet_id                   = aws_subnet.public-petsearch-subnet-1
+  vpc_security_group_ids      = [aws_security_group.bastion-sg.id]
 
   tags = {
     Name = "bastion-instance"
