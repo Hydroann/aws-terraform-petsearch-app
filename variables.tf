0c951d844c9f307eec70109d5401f9752fb80f72
Ann Kim <annkim@MacBookPro.fritz.box>
2026-05-03 04:49:17 +0200

    variables

--- variables.tf
+++ variables.tf
@@ -17,6 +17,12 @@ variable "instance_type" {
   default     = "t3.small"
 }
 
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+  default     = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
 variable "asg_min_size" {
   description = "Minimum number of Webservers. Always at least this many will be running."
   type        = number
