03e0926ca5078df8b1a9f89a8950eadc8bd0fbab
Ann Kim <annkim@MacBookPro.fritz.box>
2026-05-05 20:43:42 +0200

    autoscailing

--- autoscaling.tf
+++ autoscaling.tf
@@ -41,21 +41,7 @@ resource "aws_autoscaling_policy" "scale_out" {
   cooldown               = 300    # Then wait 5 min before scaling again
 }
 
-resource "aws_cloudwatch_metric_alarm" "cpu_high" {
-  alarm_name          = "webserver-cpu-high"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 2           # Must be high for 2 periods in a row
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 120         # Each period = 2 minutes
-  statistic           = "Average"
-  threshold           = 70          # Trigger when average CPU > 70%
-  alarm_actions       = [aws_autoscaling_policy.scale_out.arn]
 
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.petsearch-webserver.name
-  }
-}
 
 # Scale IN: remove a server when quiet
 resource "aws_autoscaling_policy" "scale_in" {
@@ -66,21 +52,6 @@ resource "aws_autoscaling_policy" "scale_in" {
   cooldown               = 300
 }
 
-resource "aws_cloudwatch_metric_alarm" "cpu_low" {
-  alarm_name          = "webserver-cpu-low"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = 2
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/EC2"
-  period              = 120
-  statistic           = "Average"
-  threshold           = 30          # Trigger when average CPU < 30%
-  alarm_actions       = [aws_autoscaling_policy.scale_in.arn]
-
-  dimensions = {
-    AutoScalingGroupName = aws_autoscaling_group.petsearch-webserver.name
-  }
-}
 
 #LAUNCH TEMPLATE AND WORDPRESS INSTALL SCRIPT
 locals {
